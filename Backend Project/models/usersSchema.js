59e772ce4fcd4344d44e8381c9458b1d67fc0b7a
Karem Ghara <57108939+KaremGhara@users.noreply.github.com>
2023-03-22 20:47:11 +0200

    some changes in FE + BE

--- Backend Project/models/usersSchema.js
+++ Backend Project/models/usersSchema.js
@@ -4,10 +4,15 @@ import mongoose from "mongoose";
 // Define a schema
 const Schema = mongoose.Schema;
 
-const dbSchema = new Schema({
-  // name: { type: String, required: true, unique: false, index: false },
+const userSchema = new Schema({
+  name: { type: String, required: true, unique: false, index: false },
+  phoneNumber: { type: String, required: true, unique: false, index: false},
+  Image: { type: String, required: true, unique: false, index: false},
+  location: { type: Number, required: true, unique: false, index: false},
+  description: { type: String, required: true, unique: false, index: false}
 });
 
-const User = mongoose.model("users", dbSchema);
+
+const User = mongoose.model("users", userSchema);
 
 export default User;
--- Flutter App/lib/screens/loading_screen.dart
+++ Flutter App/lib/screens/loading_screen.dart
@@ -1,38 +1,69 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF003249),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Image(
-              image: AssetImage('assets/imgs/logo.png'),
-              width: 200,
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-            Text(
-              '..يَداً بِيَدٍ',
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              'نَبْني مُجْتَمَعٍ أَفْضَل',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
+        child: _isLoading
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (_isLoading) // Only display CircularProgressIndicator if _isLoading is true
+                    CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      value: null,
+                    ),
+                  SizedBox(height: 20),
+                  Image(
+                    image: AssetImage('assets/imgs/logo.png'),
+                    width: 150,
+                    height: 150,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    '..يَداً بِيَدٍ',
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'نَبْني مُجْتَمَعٍ أَفْضَل',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              )
+            : null,
       ),
     );
   }
